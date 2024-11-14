cc6b316d1b0131b31f522c76a6616ce169f7b609
kshitij0504 <kshitijoza04@gmail.com>
2024-11-14 21:29:11 +0530

    temp

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://classplus.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
